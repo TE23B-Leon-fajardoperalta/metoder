938bae2bf0b62e022a9119e1ead42d556865a52b
Fajardo peralta Leon <leon.fajardoperalta@elev.ga.ntig.se>
2024-11-19 13:21:09 +0100

    first commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,42 @@
+﻿// static void Hello32()
+// {
+//     for (int i = 0; i < 32 ; i++)
+//     {
+//         Console.WriteLine("Hello, World");
+//     }
+// }
+
+
+// Hello32();
+
+// Console.ReadLine();
+// //______________________________________________________
+
+// static void fun(int n)
+// {
+//     Console.WriteLine(n * n);
+// }
+
+
+// fun(2);
+
+// Console.ReadLine();
+// // _____________________________________________________
+
+// static void Multi(float m , float n)
+// {
+//     Console.WriteLine(m * n);
+// }
+
+// Multi(2.2,4.2);
+
+// Console.ReadLine();
+
+// //______________________________________________________
+
+static void RightTriangleArea()
+{
+    
+}
+
+RightTriangleArea();
