899bdd8f1301d14e9aea4834a208d6a8c4524952
Fajardo peralta Leon <leon.fajardoperalta@elev.ga.ntig.se>
2024-11-20 15:04:37 +0100

    wow kod

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -28,15 +28,67 @@
 //     Console.WriteLine(m * n);
 // }
 
-// Multi(2.2,4.2);
+// Multi(2.2f,4.2f);
 
 // Console.ReadLine();
 
 // //______________________________________________________
 
-static void RightTriangleArea()
+// static float RightTriangleArea(float dog, float dog2)
+// {
+//     return dog * dog2;
+// }
+
+// float a = RightTriangleArea(1.1f , 2.2f);
+// Console.WriteLine(a/2);
+// Console.ReadLine();
+
+// //______________________________________________________
+// static float RightTriangleArea(float rad, float rad2)
+// {
+//     return rad * rad2;
+// }
+
+// float radrad2 = RightTriangleArea(1.1f , 1.1f);
+// Console.WriteLine(radrad2*3.14);
+
+// Console.ReadLine();
+// //______________________________________________________
+
+// static int GetNumberInput()
+// {
+
+//     bool trool = false;
+//     int result = 0;
+
+//     while (trool == false)
+//     {
+//         string read = Console.ReadLine();
+
+//         trool = int.TryParse(read, out result);
+//         if (trool == false)
+//         {
+//             Console.WriteLine("skirv igen");
+//         }
+
+//     }
+//     return result;
+
+// }
+
+
+// GetNumberInput();
+// //__________________________________________________________
+
+static string getchoice(sting1,sting2,sting3)
 {
+    string sting1 = "dog";
+    string sting2 = "dog2";
+    string sting3 = "dog3";
     
+    return sring1 + sting2 + sting3;
 }
 
-RightTriangleArea();
+getchoice();
+
+Console.ReadLine();
